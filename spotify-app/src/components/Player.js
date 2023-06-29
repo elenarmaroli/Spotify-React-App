15b7e6b848c5eb6ee169b2e1a067aa8877cfc95a
Elena <elenarmaroli@gmail.com>
2023-06-29 09:53:11 +0200

    functionality

--- spotify-app/src/components/Player.js
+++ spotify-app/src/components/Player.js
@@ -1,10 +1,10 @@
-import React, { useEffect, useState } from 'react'
+import React from 'react'
 
 const Player = ({id}) => {
 
-    return <iframe title='Player' 
+    return <iframe title='Player'
     style={{borderRadius: "8px", position: "sticky", bottom: "-50px", left: "22%", border: "0px"}}
-    src={`https://open.spotify.com/embed/track/${id}`}
+    src={`https://open.spotify.com/embed/track/${id}?autoplay=true`}
     width="1000px" 
     height="150"
     allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture" 
